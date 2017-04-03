37c82f291617e540c3e780d7e1d26c35d87b76a7
shenglong.qian <shenglong.qian@sao365.cn>
2017-04-03 12:53:02 +0800

    add node-inspector to debug

--- __test__/apply.test.js
+++ __test__/apply.test.js
@@ -1,6 +1,10 @@
 const Quo = require('../src/apply')
 describe('apply', () => {
   it('apply call', () => {
+    //this debugger for debug code, like break points. 
+    //debugger;
+    // 1. run: 'node-inspector' # to run in chrome
+    // 2. run : 'node --debug-brk ./node_modules/.bin/jest --runInBand run test '# start debug
     const statusObject = {
       status: 'A-OK'
     }
