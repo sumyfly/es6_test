4459d19a33c9e7295dcf58c1610e8a25c6616afc
shenglong.qian <shenglong.qian@gotogameday.com>
2017-06-22 11:32:40 +0800

    add rxjs more test

--- __test__/rx.test.js
+++ __test__/rx.test.js
@@ -1,7 +1,15 @@
+import { Observable } from 'rxjs'
+
 import {s} from '../src/rx'
 
 describe('rxjs test', ()=>{
-  it('test', ()=>{
-    s.subscribe(v=>console.warn(v))
+  // it('test', ()=>{
+  //   s.subscribe(v=>console.warn(v))
+  // })
+  it('test1', ()=>{
+    const  a = Observable.of(1,2,3).filter((v)=> v > 1)
+    a.subscribe(console.warn)
+    const b = a.map((v,i)=>v*2)
+    b.subscribe(console.warn)
   })
 })
