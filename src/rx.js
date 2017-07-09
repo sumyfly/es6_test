12198e5f0315d82dd4f385655253502928444cd1
shenglong.qian <shenglong.qian@gotogameday.com>
2017-07-10 01:08:01 +0800

    debug with es6, bug fixed of launch.json

--- src/rx.js
+++ src/rx.js
@@ -3,4 +3,23 @@ import { Observable } from 'rxjs'
 const s = Observable.of(1,2,3).map(x => x + '!!!')
 s.subscribe(console.warn)
 
+const a = Array(5).fill(10)
+a.map(d=>console.warn(d))
+
+const dd = (A)=>{
+  return new A()
+}
+
+function isTestable(target){
+	target.isTestable = true
+}
+
+@isTestable
+class MyTestableClass {}
+var i = 1
+let j = i * 2
+let o = j + i
+console.log(MyTestableClass.isTestable)
+console.warn(o)
+
 export {s}
